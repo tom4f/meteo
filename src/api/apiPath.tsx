1e380f003e92b27e1e9080a5e4e61cf7b8cc2e1d
Tomas Kucera <tomas.kucera1@vodafone.com>
2022-11-26 20:28:11 +0100

    synchro

--- src/api/apiPath.tsx
+++ src/api/apiPath.tsx
@@ -1,24 +1,23 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const apiPath = dev ? 'http://localhost/lipnonet/rekreace/api' : './../../api'
+export const commonPathMeteoFromFile = dev ? './../../..' : './../../..'
+export const commonPath = dev ? 'https://www.frymburk.com' : './../../..'
 
-export const commonPath = './../../..'
-
-
+export const apiPath = `${commonPath}/rekreace/api`
 
 class urlParams {
     static get getSearchObj() {
         const search = window.location.search.substring(1)
-        const searchObj: { [key: string] : string } = {}
+        const searchObj: { [key: string]: string } = {}
         const searchArr = search.split('&')
-        !!searchArr[0] && searchArr.forEach( (ittem) => {
-            const [ key, value ] = ittem.split('=')
-            searchObj[key] = value 
+        !!searchArr[0] && searchArr.forEach((ittem) => {
+            const [key, value] = ittem.split('=')
+            searchObj[key] = value
         })
         return {
             fotoGalleryOwner: searchObj.fotoGalleryOwner || '_ubytovani',
-            editStatus      : searchObj.edit === 'yes' ? true : false,
-            category        : searchObj.category === 'yes' ? true : false
+            editStatus: searchObj.edit === 'yes' ? true : false,
+            category: searchObj.category === 'yes' ? true : false
         }
     }
 }
--- src/api/meteoFromFile.tsx
+++ src/api/meteoFromFile.tsx
@@ -1,13 +1,13 @@
-import { commonPath } from './apiPath'
+import { commonPathMeteoFromFile } from './apiPath'
 import { loadDataFunctionType, pureData } from '../components/TypeDefinition'
 
-export const loadPocasiAsync: loadDataFunctionType = async (start, end, index, graphsConfig ) => {
+export const loadPocasiAsync: loadDataFunctionType = async (start, end, index, graphsConfig) => {
 
-    try { 
+    try {
 
         // get meteodata array for one file
-        const loadOneFile = async ( txtFile: string ) => {
-            const response = await fetch( txtFile )
+        const loadOneFile = async (txtFile: string) => {
+            const response = await fetch(txtFile)
             // this response check not works if .httaccess show default html page instead text file
             if (response.status !== 200) return []
             const text = await response.text()
@@ -18,7 +18,7 @@ export const loadPocasiAsync: loadDataFunctionType = async (start, end, index, g
             arr.shift()
             arr.shift()
             // return empty arr if not valid text file - return (-1) = FALSE
-            return !arr[0].search( /..\...\.......:../ ) ? arr : []
+            return !arr[0].search(/..\...\.......:../) ? arr : []
         }
 
 
@@ -26,23 +26,23 @@ export const loadPocasiAsync: loadDataFunctionType = async (start, end, index, g
         // create filePaths array with correct days order
         let meteoFiles: string[] = [];
         const dayOfWeekNow = new Date().getUTCDay();
-        for ( let day = dayOfWeekNow + 1; day < dayOfWeekNow + 6; day++ ) {
-            const correctedDay = day > 6 ? day - 7 :  day;
-            const meteoFile = `${commonPath}/davis/archive/downld02-${ correctedDay }.txt`;
-            meteoFiles = [ ...meteoFiles, meteoFile ];
+        for (let day = dayOfWeekNow + 1; day < dayOfWeekNow + 6; day++) {
+            const correctedDay = day > 6 ? day - 7 : day;
+            const meteoFile = `${commonPathMeteoFromFile}/davis/archive/downld02-${correctedDay}.txt`;
+            meteoFiles = [...meteoFiles, meteoFile];
         }
-        meteoFiles = [ ...meteoFiles, `${commonPath}/davis/downld02.txt` ];
+        meteoFiles = [...meteoFiles, `${commonPathMeteoFromFile}/davis/downld02.txt`];
 
 
 
         // create meteo array for all 7 days
         let mergedArr: string[] = [];
-        const myPromises = meteoFiles.map( filePath => loadOneFile( filePath ) )
-        await Promise.all( myPromises )
-            .then( responses => responses.forEach(
-                response => mergedArr = [ ...mergedArr, ...response ]
+        const myPromises = meteoFiles.map(filePath => loadOneFile(filePath))
+        await Promise.all(myPromises)
+            .then(responses => responses.forEach(
+                response => mergedArr = [...mergedArr, ...response]
             ))
-        
+
 
         // in react folder: /public/davis/downld02.txt
 
@@ -51,62 +51,62 @@ export const loadPocasiAsync: loadDataFunctionType = async (start, end, index, g
         //const arrOfArr = arr.map( line => line.split(/  +/g) )
 
         const dirObj: { [key: string]: number } = {
-            '---' : 16,
-            'NNW' : 15,
-            'NW' : 14,
-            'WNW' : 13,
-            'W' : 12,
-            'WSW' : 11,
-            'SW' : 10,
-            'SSW' : 9,
-            'S' : 8,
-            'SSE' : 7,
-            'SE' : 6,
-            'ESE' : 5,
-            'E' : 4,
-            'ENE' : 3,
-            'NE' : 2,
-            'NNE' : 1,
-            'N' : 0
-       }
+            '---': 16,
+            'NNW': 15,
+            'NW': 14,
+            'WNW': 13,
+            'W': 12,
+            'WSW': 11,
+            'SW': 10,
+            'SSW': 9,
+            'S': 8,
+            'SSE': 7,
+            'SE': 6,
+            'ESE': 5,
+            'E': 4,
+            'ENE': 3,
+            'NE': 2,
+            'NNE': 1,
+            'N': 0
+        }
 
-        const arrOfObj = mergedArr.reduce( ( accumulator: Array<pureData>, line, index ) => {
-            
+        const arrOfObj = mergedArr.reduce((accumulator: Array<pureData>, line, index) => {
+
             const arrFromLine = line.trim().split(/ +/g);
 
-            const names = [ 'OrigDate', 'Time', 'TempOut', 'TempHi', 'TempLow', 'HumOut', 'DewPt', 'WindSpeed', 'WindDir', 'WindRun', 'HiSpeed', 'HiDir', 'WindChill', 'HeatIndex', 'THWIndex', 'Bar', 'Rain', 'RainRate', 'HeatDD', 'CoolDD', 'TempIn', 'HumIn', 'DewIn', 'HeatIn', 'EMCIn', 'AirDensityIn', 'WindSamp', 'WindTx', 'ISSRecept', 'ArcInt' ];
+            const names = ['OrigDate', 'Time', 'TempOut', 'TempHi', 'TempLow', 'HumOut', 'DewPt', 'WindSpeed', 'WindDir', 'WindRun', 'HiSpeed', 'HiDir', 'WindChill', 'HeatIndex', 'THWIndex', 'Bar', 'Rain', 'RainRate', 'HeatDD', 'CoolDD', 'TempIn', 'HumIn', 'DewIn', 'HeatIn', 'EMCIn', 'AirDensityIn', 'WindSamp', 'WindTx', 'ISSRecept', 'ArcInt'];
 
-            const objFromLine: { [key: string]: string | number } = arrFromLine.reduce( (accObj, value, index) => ( { ...accObj, [names[index]]: value } ), {})
+            const objFromLine: { [key: string]: string | number } = arrFromLine.reduce((accObj, value, index) => ({ ...accObj, [names[index]]: value }), {})
 
-            const [ OrigDate, Time] = arrFromLine;
+            const [OrigDate, Time] = arrFromLine;
 
             // UTC used to disable time offset effect
-            const [ day, month, year ] = OrigDate.split('.');
-            const [ hour, minute ] = Time.split(':');
-            const dateString = new Date(Date.UTC( 2000 + (+year), (+month) - 1, +day, +hour, +minute) ).toJSON();
-            objFromLine.Date    = dateString;
+            const [day, month, year] = OrigDate.split('.');
+            const [hour, minute] = Time.split(':');
+            const dateString = new Date(Date.UTC(2000 + (+year), (+month) - 1, +day, +hour, +minute)).toJSON();
+            objFromLine.Date = dateString;
             // Wind dir - degrees from string
-            objFromLine.WindDir = 22.5 * dirObj[ objFromLine.WindDir ];
-    
+            objFromLine.WindDir = 22.5 * dirObj[objFromLine.WindDir];
+
             // tricky index is from 1
             // skip duplicated entries when merging more text files ( optional? )
-            const result = index > 0 && objFromLine.Date < accumulator[ accumulator.length - 1 ].Date
-                         ? accumulator
-                         : [ ...accumulator, objFromLine ];
+            const result = index > 0 && objFromLine.Date < accumulator[accumulator.length - 1].Date
+                ? accumulator
+                : [...accumulator, objFromLine];
 
             return result
         }, [])
 
         if (arrOfObj.length === 0) return graphsConfig;
 
-        const graphsData = ( [{ ...graphsConfig[0], data: arrOfObj }] )
-       
+        const graphsData = ([{ ...graphsConfig[0], data: arrOfObj }])
+
         return graphsData
 
     }
-        catch (err) {
-            console.log(err)
-            return graphsConfig;
-        }
+    catch (err) {
+        console.log(err)
+        return graphsConfig;
+    }
 
 }
